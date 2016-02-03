89a301c62e279490509925018568bc3ffa70cdfc
assertivist <andy.halstead@gmail.com>
2016-02-03 10:56:40 -0700

    initial commit!

---
+++ vastan/Assets/GeomBuilder.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GeomBuilder {
+
+    private Mesh m;
+    private List<Vector3> new_verts;
+    private List<int> new_triangles;
+
+    public void init ()
+    {
+        m = new Mesh();
+        new_verts = new List<Vector3>();
+        new_triangles = new List<int>();
+    }
+
+    public void add_rect (Color c, Vector3 p1, Vector3 p2)
+    {
+        bool swap_y = p1.x != p2.y;
+
+        new_verts.Add(p1);
+
+        if (swap_y)
+            new_verts.Add(new Vector3(p2.x, p1.y, p1.z));
+        else
+            new_verts.Add(new Vector3(p2.x, p2.y, p1.z));
+
+        new_verts.Add(p2);
+
+        if (swap_y)
+            new_verts.Add(new Vector3(p1.x, p2.y, p2.z));
+        else
+            new_verts.Add(new Vector3(p1.x, p1.y, p2.z));
+
+
+        int len = new_verts.Count;
+        new_triangles.Add(len - 4);
+        new_triangles.Add(len - 3);
+        new_triangles.Add(len - 2);
+
+        new_triangles.Add(len - 2);
+        new_triangles.Add(len - 3);
+        new_triangles.Add(len - 4);
+    }
+
+    public Mesh build_mesh ()
+    {
+        m = new Mesh();
+        m.vertices = new_verts.ToArray();
+        m.triangles = new_triangles.ToArray();
+        return m;
+    }
+	
+}
---
+++ vastan/Assets/GeomTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeomTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GeomBuilder gb = new GeomBuilder();
+        gb.init();
+        gb.add_rect(new Color(200, 34, 34), new Vector3(1, 1, 1), new Vector3(-1, -1, -1));
+        Mesh m = gb.build_mesh();
+        GetComponent<MeshFilter>().mesh = m;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(0, 20, 0);
+	}
+}
