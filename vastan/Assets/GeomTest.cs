fdeef75195b84a3a0e7e2882d642eb565dfa0622
ahalstead <andy.halstead@gmail.com>
2016-02-08 05:26:46 -0700

    mostly working geombuilder

--- vastan/Assets/GeomTest.cs
+++ vastan/Assets/GeomTest.cs
@@ -1,19 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml;
 
 public class GeomTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        test_xml();
+    }
+
+    void test() {
         GeomBuilder gb = new GeomBuilder();
         gb.init();
-        gb.add_rect(new Color(200, 34, 34), new Vector3(1, 1, 1), new Vector3(-1, -1, -1));
-        Mesh m = gb.build_mesh();
+        Quaternion rot = Quaternion.identity;
+        rot.eulerAngles = new Vector3(0, 0, 0);
+        var pos = new Vector3(0, 0, 0);
+        var size = new Vector3(2, 2, 2);
+        var black = new Color(.75f, .2f, .2f);
+
+        gb.add_block(black, pos, size, rot);
+
+        pos = new Vector3(0, 1, 2);
+        size = new Vector3(2, 4, 2);
+        gb.add_block(black, pos, size, rot);
+
+        pos = new Vector3(2, 2, 0);
+        size = new Vector3(2, 6, 2);
+        gb.add_block(black, pos, size, rot);
+
+        pos = new Vector3(0, -1, 0);
+        size = new Vector3(100, 1, 100);
+        gb.add_block(new Color(.1f, .1f, .3f, .2f), pos, size, rot);
+
+        Mesh m = gb.get_mesh();
         GetComponent<MeshFilter>().mesh = m;
-	}
+    }
+
+    void test_xml() {
+        Level l = new Level();
+        l.load("stratocaster");
+        GameObject l_root = l.game_object();
+        l_root.transform.SetParent(transform);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        //transform.Rotate(0, 20, 0);
+        transform.Rotate(0, .01f, 0);
 	}
 }
---
+++ vastan/Assets/Level.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level {
+
+    public string name;
+    public string tagline;
+    public string author;
+    public string description;
+
+    public List<Mesh> statics;
+    public List<Mesh> holograms;
+
+    private GameObject static_fab;
+
+    private GameObject parent = new GameObject("LevelRoot");
+
+    private GeomBuilder current_gb = new GeomBuilder();
+    private bool current_is_hologram = false;
+
+    public void load(string level) {
+        TextAsset ta = (TextAsset)Resources.Load(level);
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(ta.text);
+        XmlNode mapnode = doc.DocumentElement;
+        name = parse_string(mapnode, "name");
+        tagline = parse_string(mapnode, "tagline");
+        author = parse_string(mapnode, "author");
+        description = parse_string(mapnode, "description");
+        Debug.Log("Loading Level " + name);
+
+        static_fab = Resources.Load("LevelGeometry", typeof(GameObject)) as GameObject;
+
+        current_gb.init();
+        statics = new List<Mesh>();
+        holograms = new List<Mesh>();
+
+        parse_node(mapnode);
+    }
+
+    private void cycle_mesh(bool is_hologram) {
+        if (current_gb != null) {
+            if (is_hologram) {
+                holograms.Add(current_gb.get_mesh());
+                current_gb = new GeomBuilder();
+                current_gb.init();
+            }
+            else {
+                statics.Add(current_gb.get_mesh());
+                current_gb = new GeomBuilder();
+                current_gb.init();
+            }
+        }
+        else {
+            current_gb = new GeomBuilder();
+            current_gb.init();
+        }
+    }
+
+    private void parse_node(XmlNode parent_node)  {
+        foreach (XmlNode node in parent_node.ChildNodes) {
+            switch (node.Name)  {
+
+                case "static":
+                    cycle_mesh(current_is_hologram);
+                    parse_node(node);
+                    break;
+
+                case "hologram":
+                    //cycle_mesh(current_is_hologram);
+                    //current_is_hologram = true;
+                    parse_node(node);
+                    break;
+
+                case "block":
+                    parse_block(node);
+                    break;
+                case "ramp":
+                    parse_ramp(node);
+                    break;
+                case "wedge":
+                    parse_wedge(node);
+                    break;
+                case "dome":
+                    parse_dome(node);
+                    break;
+                case "ground":
+                    parse_ground(node);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    private void parse_block(XmlNode block_node) {
+        Vector3 center = parse_vec3(block_node.Attributes["center"]);
+        Vector3 size = parse_vec3(block_node.Attributes["size"]);
+        Color c = parse_color(block_node);
+        Quaternion rot = Quaternion.identity;
+
+        Vector3 angles = rot.eulerAngles;
+        angles.x += parse_float(block_node, "pitch");
+        angles.y += parse_float(block_node, "yaw");
+        angles.z += parse_float(block_node, "roll");
+        rot.eulerAngles = angles;
+        
+        current_gb = current_gb.add_block(c, center, size, rot);
+        
+    }
+    private void parse_ramp(XmlNode ramp_node) {
+        Vector3 ramp_base = parse_vec3(ramp_node.Attributes["base"]);
+        Vector3 ramp_top = parse_vec3(ramp_node.Attributes["top"]);
+        float width = parse_float(ramp_node, "width");
+        float thickness = parse_float(ramp_node, "thickness");
+        Color c = parse_color(ramp_node);
+        Quaternion rot = Quaternion.identity;
+
+        current_gb.add_ramp(c, ramp_base, ramp_top, width, thickness, rot);
+    }
+    private void parse_wedge(XmlNode wedge_node) {
+        Vector3 wedge_base = parse_vec3(wedge_node.Attributes["base"]);
+        Vector3 wedge_top = parse_vec3(wedge_node.Attributes["top"]);
+        float width = parse_float(wedge_node, "width");
+        Color c = parse_color(wedge_node);
+        Quaternion rot = Quaternion.identity;
+
+        current_gb.add_wedge(c, wedge_base, wedge_top, width, rot);
+
+    }
+    private void parse_dome(XmlNode dome_node) {
+
+    }
+
+    private void parse_ground(XmlNode ground_node) {
+        Color c = parse_color(ground_node);
+        current_gb.add_block(c, new Vector3(0, -1f, 0), new Vector3(1000, 1, 1000), Quaternion.identity);
+    }
+
+    private Color parse_color(XmlNode node) {
+        XmlAttribute x = node.Attributes["color"];
+        if (x == null) return new Color(1, 1, 1);
+        string value = x.Value;
+        string[] values = value.Split(',');
+        float r = float.Parse(values[0]);
+        float g = float.Parse(values[1]);
+        float b = float.Parse(values[2]);
+        return new Color(r, g, b);
+    }
+
+    private Vector3 parse_vec3(XmlAttribute vec3_attrib) {
+        string value = vec3_attrib.Value;
+        string[] values = value.Split(',');
+        float x = float.Parse(values[0]);
+        float y = float.Parse(values[1]);
+        float z = float.Parse(values[2]);
+        return new Vector3(x, y, z);
+    }
+
+    private string parse_string(XmlNode n, string key) {
+        if (n.Attributes[key] == null) {
+            return "";
+        }
+        else return n.Attributes[key].Value;
+    }
+
+    private float parse_float(XmlNode n, string key) {
+        if (n.Attributes[key] == null) {
+            return 0;
+        }
+        else return float.Parse(n.Attributes[key].Value);
+    }
+
+    public GameObject game_object() {
+        cycle_mesh(current_is_hologram);
+        foreach (Mesh m in statics) {
+            GameObject geom = GameObject.Instantiate(static_fab, Vector3.zero, Quaternion.identity) as GameObject;
+            geom.GetComponent<MeshFilter>().mesh = m;
+            geom.transform.SetParent(parent.transform);
+        }
+        foreach (Mesh m in holograms) {
+            GameObject geom = GameObject.Instantiate(static_fab, Vector3.zero, Quaternion.identity) as GameObject;
+            geom.GetComponent<MeshFilter>().mesh = m;
+            geom.transform.SetParent(parent.transform);
+        }
+        return parent;
+    }
+}
