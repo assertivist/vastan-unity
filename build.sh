6769cb0103c8f4362465fcb4992bfb44b7228cb9
assertivist <andy.halstead@gmail.com>
2017-01-29 18:52:36 -0700

    testing updates to CI configuration

--- build.sh
+++ build.sh
@@ -1,36 +1,37 @@
 #!/bin/sh
 
 project="vastan"
+filename="$project-$TRAVIS_COMMIT"
+unity_exe="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+options="-batchmode -nographics -silent-crashes -projectPath $(pwd)/$project"
+workspace="$(pwd)/Build"
+packages="$(pwd)/Package"
+myhome="$(pwd)"
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/Build/windows/build.log \
-  -projectPath $(pwd)/$project \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+mkdir workspace
+mkdir packages
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/Build/osx/build.log \
-  -projectPath $(pwd)/$project \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
+build_for_target()
+{
+  target=$1
+  unitytarget=$2
+  extension=$3
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/Build/linux/build.log \
-  -projectPath $(pwd)/$project \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+  echo "Attempting to build $project for $target"
+  $unity_exe \
+    $options \
+    -logFile $(pwd)/Build/$target/build.log \
+    -$unitytarget "$workspace/$target/$filename.$extension" \
+    -quit
 
-find ./Build/
+  cd $workspace
+  zip -r9 $packages/$target.zip $target
+  cd $myhome
+}
+
+build_for_target windows buildWindowsPlayer exe
+build_for_target osx buildOSXUniversalPlayer app
+build_for_target linux buildLinuxUniversalPlayer exe
+
+find $packages
 
