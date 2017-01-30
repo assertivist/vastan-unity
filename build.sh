11c19a1fea708cb9c7dab33555c95c5b544dbf8b
assertivist <andy.halstead@gmail.com>
2017-01-29 17:33:38 -0700

    don't output log files but instead sync them along with the builds

--- build.sh
+++ build.sh
@@ -7,37 +7,30 @@ echo "Attempting to build $project for Windows"
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
+  -logFile $(pwd)/Build/windows/build.log \
   -projectPath $(pwd)/$project \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
-
-echo 'Logs from WINDOWS build'
-cat $(pwd)/unity.log
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
+  -logFile $(pwd)/Build/osx/build.log \
   -projectPath $(pwd)/$project \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
-
-echo 'Logs from MAC build'
-cat $(pwd)/unity.log
 
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity.log \
+  -logFile $(pwd)/Build/linux/build.log \
   -projectPath $(pwd)/$project \
   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
-
-echo 'Logs from LINUX build'
+  
 find .
-cat $(pwd)/unity.log
+
