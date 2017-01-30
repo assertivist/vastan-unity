96b5189062a0a369312d33b9936869d014c14743
assertivist <andy.halstead@gmail.com>
2017-01-29 19:49:11 -0700

    testing updates to CI configuration

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-package_dir=$TRAVIS_BUILD_DIR/Package/
-deploy_target=vastan@vastan.net:/home/vastan/public_html/release
+package_dir="$TRAVIS_BUILD_DIR/Package/"
+deploy_target="vastan@vastan.net:/home/vastan/public_html/release/$1"
 
-rsync -r --delete-after -e 'ssh -o StrictHostKeyChecking=no' $package_dir $deploy_target/$1
+rsync -r --delete-after -e 'ssh -o StrictHostKeyChecking=no' $package_dir $deploy_target
