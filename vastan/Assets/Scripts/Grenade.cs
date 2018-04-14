d41bc272cfb54a0eded5f1baf568aa14aa22451f
assertivist <andy.halstead@gmail.com>
2018-04-14 00:12:25 -0600

    shuffled kinematic field on some prefabs, grenades now use rigidbodies and are not triggers etc., better splash damage force application, still not totally right

--- vastan/Assets/Scripts/Grenade.cs
+++ vastan/Assets/Scripts/Grenade.cs
@@ -14,7 +14,9 @@ public class Grenade : Projectile {
 
     public Vector3 speed = new Vector3(0, 0, 0);
     public float attack_time;
-    
+
+    HashSet<GameObject> hit_things = new HashSet<GameObject>();
+
     void Start () {
         
         exp_colors = new List<Color> {
@@ -46,7 +48,7 @@ public class Grenade : Projectile {
 
         //explode after 100 "frames"
         if ((Time.time - attack_time) * Game.AVARA_FPS > 100) {
-            asplode_force();
+            splash_force();
             asplode();
         }
     }
@@ -66,8 +68,8 @@ public class Grenade : Projectile {
     }
 
 
-    void OnTriggerEnter(Collider other) {
-        var sc = other.gameObject.GetComponent<SceneCharacter>();
+    void OnCollisionEnter(Collision c) {
+        var sc = c.collider.gameObject.GetComponent<SceneCharacter>();
         if (sc != null)
         {
             var other_id = sc.BaseCharacter.Id;
@@ -79,43 +81,70 @@ public class Grenade : Projectile {
         }
         //Debug.Log("fired by: " + fired_by);
         //Debug.Log("hit: " + other.gameObject.GetInstanceID() + other.gameObject.name);
-        asplode_force();
+        direct_hit(c.contacts);
 		asplode();
 	}
+
+    void direct_hit(ContactPoint[] contacts) {
+        foreach (ContactPoint contact in contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            //do maximum force application for these contact points
+            var other = contact.otherCollider.gameObject;
+            hit_things.Add(other);
+            var dist = contact.normal * -.001f;
+            var closest_hitpower = Projectile.explosion_scale(power, dist);
+            apply_force_to_gameobject(closest_hitpower, other, dist);
+        }
+        // now do splash damage
+        splash_force();
+    }
                               
-	void asplode_force() {
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 6f);
+	void splash_force() {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 6f);
 		int i = 0;
 		while (i < hitColliders.Length)
 		{
 			var go = hitColliders[i].gameObject;
             if (go == gameObject) {
+                // don't apply force to myself (grenade prefab)
                 i++;
                 continue;
             }
-			var dist = go.transform.position - transform.position;
+            if (hit_things.Contains(go)) {
+                // we already applied max force from hitting this,
+                // so skip it
+                i++;
+                continue;
+            }
+            var closest_point = hitColliders[i].ClosestPointOnBounds(transform.position);
+            var dist = closest_point - transform.position;
             Debug.Log(go + " distance from explosion: " + dist);
-			var hitpower = Projectile.explosion_scale(power, dist);
-
-			var hit_sc = go.GetComponent<SceneCharacter3D>();
-			if (hit_sc != null) {
-				if (dist.y < 0)
-					hit_sc.crouch_spring.vel -= dist.normalized.y * hitpower;
-                
-				hit_sc.state.momentum += dist.normalized * hitpower;
-                hit_sc.was_hit(hitpower, power);
-			}
-
-			var hit_p = go.GetComponent<Projectile>();
-			if (hit_p != null)
-				hit_p.peterout();
-
-            var hit_s = go.GetComponent<Static>();
-            if (hit_s != null) 
-                hit_wall = true;
-            
+            var hitpower = Projectile.explosion_scale(power, dist);
+            apply_force_to_gameobject(hitpower, go, dist);
 
 			i++;
 		}
 	}
+    
+
+    void apply_force_to_gameobject(float hitpower, GameObject go, Vector3 dist) {
+        
+        var hit_sc = go.GetComponent<SceneCharacter3D>();
+        if (hit_sc != null) {
+            if (dist.y < 0)
+                hit_sc.crouch_spring.vel -= dist.normalized.y * hitpower;
+
+            hit_sc.state.momentum += dist.normalized * hitpower;
+            hit_sc.was_hit(hitpower, power);
+        }
+
+        var hit_p = go.GetComponent<Projectile>();
+        if (hit_p != null)
+            hit_p.peterout();
+
+        var hit_s = go.GetComponent<Static>();
+        if (hit_s != null)
+            hit_wall = true;
+
+    }
 }
