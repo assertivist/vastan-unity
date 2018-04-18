bce51fe7d4d94cf277ebb89b6cc440a896f88f56
ahalstead <andy.halstead@gmail.com>
2018-04-18 14:20:58 -0600

    Destroy sound script

---
+++ vastan/Assets/Sounds/DestroySound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource mysound = GetComponent<AudioSource>();
+        Destroy(gameObject, mysound.clip.length);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
